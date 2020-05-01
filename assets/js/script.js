1a44e0a4fb2cadec54850cf17e54be94ccc8fdaa
lreshmaprdxn <reshmal@prdxn.com>
2020-05-01 21:00:13 +0530

    script page scripting added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,11 @@
-/* Author: 
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let start = document.querySelector('.start');
+start.addEventListener('click', showQuiz);
+function showQuiz() { 
+  let name = document.getElementById('userName').value;
+  if(name === '') {
+    alert('First enter your name...');
+  } else {
+    sessionStorage.setItem('name', name);
+    location.href = "quiz.html";
+  }  
+}
